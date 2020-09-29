a5cf2d26721b5c512b5ef5f2f0d50ac5229cd4d8
user <user@ED11>
2020-09-29 09:33:50 +0800

    homework

--- src/abc.java
+++ src/abc.java
@@ -1,6 +1,8 @@
 
 public class abc {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("I'm Lee Yan De");
+		System.out.println("MY NID is D0862965");
+		System.out.println("This is lab 1 hw");
 	}
 }
